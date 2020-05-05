54249e2cdc10794a542a0c7205b7699427f1bf1a
= <=>
2020-05-06 00:10:55 +0700

    update sp header

--- index.php
+++ index.php
@@ -9,9 +9,10 @@ require_once DOC_ROOT.'/vipphone/common/php/common.php';
 <head>
     <?=insertHeadInfo();?>
     <?=insertCSS('/vipphone/top/css/style.css');?>
+    <?=insertJS('/vipphone/top/js/script.js');?>
 </head>
 <body>
-    <header>
+    <header class="only-pc">
         <div class="Responsive">
             <div class="Menu">
                 <div class="Menu-branch">
@@ -19,9 +20,9 @@ require_once DOC_ROOT.'/vipphone/common/php/common.php';
                         <img src="/vipphone/common/img/logo.png" alt="">
                     </div>
                     <div class="Menu-branch-search">
-                        <div class="Menu-branch-search-location">
+                        <!--<div class="Menu-branch-search-location">
                             <p class="Menu-branch-search-location_txt">Hồ Chí Minh </p>
-                        </div>
+                        </div> -->
                         <div class="Menu-branch-search-input">
                             <input type="text" name="" value="" placeholder="Tìm kiếm...">
                         </div>
@@ -31,27 +32,33 @@ require_once DOC_ROOT.'/vipphone/common/php/common.php';
                     </div>
                 </div>
                 <div class="Menu-list">
+                    <a href="/vipphone/register.php" class="Menu-list_link">
                     <div class="Menu-list-item">
                         <div class="Menu-list-item-content">
                             <div class="Menu-list-item-content_icon"><i class="fas fa-home"></i></div>
                             <p class="Menu-list-item-content_txt">TRANG CHỦ</p>
                         </div>
                     </div>
+                    </a>
+                    <a href="/vipphone/register.php" class="Menu-list_link">
                     <div class="Menu-list-item">
                         <div class="Menu-list-item-content">
                             <div class="Menu-list-item-content_icon"><i class="fas fa-info"></i></div>
                             <p class="Menu-list-item-content_txt">GIỚI THIỆU</p>
                         </div>
                     </div>
+                    </a>
+                    <a href="/vipphone/register.php" class="Menu-list_link">
                     <div class="Menu-list-item">
                         <div class="Menu-list-item-content">
                             <div class="Menu-list-item-content_icon"><i class="fas fa-mobile-alt"></i></div>
                             <p class="Menu-list-item-content_txt">SẢN PHẨM</p>
                         </div>
+                    </a>
                         <div class="Menu-list-item-sub">
                             <div class="Menu-list-item-sub-list">
                                 <div class="Menu-list-item-sub-list-item">
-                                    <a href="#" class="Menu-list-item-sub-list-item_txt">Iphone</a>
+                                    <a href="/vipphone/register.php" class="Menu-list-item-sub-list-item_txt">Iphone</a>
                                 </div>
                                 <div class="Menu-list-item-sub-list-item">
                                     <a href="#" class="Menu-list-item-sub-list-item_txt">SamSung</a>
@@ -71,30 +78,72 @@ require_once DOC_ROOT.'/vipphone/common/php/common.php';
                             </div>
                         </div>
                     </div>
+                    <a href="/vipphone/register.php" class="Menu-list_link">
                     <div class="Menu-list-item">
                         <div class="Menu-list-item-content">
                             <div class="Menu-list-item-content_icon"><i class="far fa-newspaper"></i></div>
                             <p class="Menu-list-item-content_txt">TIN TỨC</p>
                         </div>
                     </div>
+                    </a>
+                    <a href="/vipphone/register.php" class="Menu-list_link">
                     <div class="Menu-list-item">
                         <div class="Menu-list-item-content">
                             <div class="Menu-list-item-content_icon"><i class="far fa-id-card"></i></div>
                             <p class="Menu-list-item-content_txt">LIÊN HỆ</p>
                         </div>
                     </div>
+                    <a href="/vipphone/register.php" class="Menu-list_link">
+                    <div class="Menu-list-item">
+                        <div class="Menu-list-item-content">
+                            <div class="Menu-list-item-content_icon"><i class="fas fa-shopping-cart"></i></div>
+                            <p class="Menu-list-item-content_txt">GIỎ HÀNG<span class="count">(0)</span></p>
+                        </div>
+                    </div>
+                    </a>
+                    </a>
+                    <a href="/vipphone/register.php" class="Menu-list_link">
                     <div class="Menu-list-item">
                         <div class="Menu-list-item-content">
                             <div class="Menu-list-item-content_icon"><i class="fas fa-user"></i></div>
                             <p class="Menu-list-item-content_txt">ĐĂNG NHẬP</p>
                         </div>   
                     </div>
+                    </a>
+                    <!--<a href="/vipphone/register.php" class="Menu-list_link">
                     <div class="Menu-list-item">
                         <div class="Menu-list-item-content">
-                            <div class="Menu-list-item-content_icon"><i class="fas fa-key"></i></i></div>
+                            <div class="Menu-list-item-content_icon"><i class="fas fa-key"></i></div>
                             <p class="Menu-list-item-content_txt">ĐĂNG KÍ</p>
                         </div>
                     </div>
+                    </a>-->
+                    
+                </div>
+            </div>
+        </div>
+    </header>
+    <header class="only-sp">
+        <div class="js-nav Nav">
+            <div class="Nav_logo">
+                <img src="/vipphone/common/img/logo.png" alt="logo">
+            </div>
+            <div class="Nav-search">
+                <div class="Nav-search-content">
+                    <div class="Nav-search-content_input">
+                        <input type="text" placeholder="Tìm kiếm">
+                    </div>
+                    <div class="Nav-search-content_button">
+                        <i class="fa fa-search"></i>
+                    </div>
+                </div>
+               
+            </div>
+            <div class="Nav-hamburger">
+                <div class="js-navHamburger Nav-hamburger-content">
+                    <span class="Nav-hamburger-content_icon"></span>
+                    <span class="Nav-hamburger-content_icon"></span>
+                    <span class="Nav-hamburger-content_icon"></span>
                 </div>
             </div>
         </div>
---
+++ top/js/script.js
@@ -0,0 +1,9 @@
+(function($) {
+    $(function() {
+
+        $('.js-navHamburger').click(function(){
+             $('.js-nav').toggleClass('Nav--active')
+        });
+    });
+    
+})(jQuery);
